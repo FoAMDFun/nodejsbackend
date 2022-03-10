36e4b3b3a9fc44f60cd7832ba4796021ebe2a3ac
Cseh Krisztián (FoAMDFun) <foamdfun@gmail.com>
2022-03-10 11:20:48 +0100

    Basic routing works

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require("express");
+
+const bodyParser = require("body-parser");
+
+const adminRoutes = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
+const notFoundRoutes = require("./routes/404");
+
+const app = express();
+
+// Middelware, to parse body from req. It extracts data into an object, so the console log on the product route
+// gives back a { title: 'something' } type object with key vaule pairs
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use("/admin", adminRoutes);
+app.use(shopRoutes);
+app.use(notFoundRoutes);
+
+app.listen(3000);
---
+++ routes/admin.js
@@ -0,0 +1,18 @@
+const path = require("path");
+
+const express = require("express");
+
+const router = express.Router();
+
+// Route handling routes
+
+router.get("/add-product", (req, res) => {
+  res.sendFile(path.join(__dirname, "../views/add-product.html"));
+});
+
+router.post("/add-product", (req, res) => {
+  console.log(req.body);
+  res.redirect("/");
+});
+
+module.exports = router;
